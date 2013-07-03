6a587596532f4973890aae99ba0516d34427673b
guilherme.p.pereira <guilherme.p.pereira@gmail.com>
2013-07-03 02:53:43 +0000

    Acrescentado: telas, arquivo de configuração do hibernate, mapeamento de entidades e classe DAO genérica.

---
+++ MARCO 2 18-07/codes/contpatri-web/src/main/java/br/ufg/inf/es/fs/contpatri/web/persistencia/dao/GenericDAO.java
@@ -0,0 +1,48 @@
+/**
+ * Esse documento é parte do código fonte e artefatos relacionados ao projeto
+ * CONTPATRI, em desenvolvimento pela Fábrica de Software da UFG.
+ *
+ * Links relevantes: Fábrica de Software: http://fs.inf.ufg.br/ Instituto de
+ * Informática UFG: http://www.inf.ufg.br Projeto CONTPATRI DROPBOX:
+ * https://www.dropbox.com/home/CONTPATRI%20-%20012013 Projeto CONTPATRI
+ * REDMINE:
+ *
+ * Copyleft © UFG.
+ *
+ * Licenciado sobre a licença GNU-GPL v3
+ *
+ * Você pode obter uma cópia da licença em http://www.gnu.org/licenses/gpl.html
+ *
+ * A menos que especificado ou exigido por legislação local, o software é
+ * fornecido "da maneira que está", sem garantias ou condições de qualquer tipo,
+ * nem expressas nem implícitas. Em caso de dúvidas referir a licença GNU-GPL.
+ */
+package br.ufg.inf.es.fs.contpatri.web.persistencia.dao;
+
+/**
+ * Interface DAO genérica.
+ * 
+ * @author Guilherme de Paula <guilherme.p.pereira at gmail.com>
+ */
+import java.io.*;
+import java.util.*;
+import org.hibernate.Query;
+
+interface GenericDAO<T, ID extends Serializable> {
+
+    public void create(T entity);
+    
+    public void update(T entity);
+
+    public void merge(T entity);
+
+    public void delete(T entity);
+
+    public List<T> findMany(Query query);
+
+    public T findOne(Query query);
+
+    public List findAll(Class clazz);
+
+    public T findByID(Class clazz, int id);
+}
