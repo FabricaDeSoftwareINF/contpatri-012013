d7cd76b57f1e45ffaed795309ba6deff7fba001d
daynilou@gmail.com <daynilou@gmail.com@060b68a2-cbdf-6f48-7e40-96d2eb126271>
2013-07-11 23:54:20 +0000

    Revert do HibernateUtil.

--- trunk/MARCO 2 18-07/codes/contpatri-web/src/main/java/br/ufg/inf/es/fs/contpatri/web/persistencia/HibernateUtil.java
+++ trunk/MARCO 2 18-07/codes/contpatri-web/src/main/java/br/ufg/inf/es/fs/contpatri/web/persistencia/HibernateUtil.java
@@ -26,53 +26,49 @@ import org.hibernate.cfg.AnnotationConfiguration;
 /**
  * Hibernate Utility class with a convenient method to get Session Factory
  * object.
- * 
+ *
  * @author Guilherme de Paula <guilherme.p.pereira at gmail.com>
  */
 public class HibernateUtil {
 
-	private static final SessionFactory SESSION_FACTORY;
+    private static final SessionFactory sessionFactory;
 
-	private HibernateUtil() {
-	}
+    static {
+        try {
+            // Create the SessionFactory from standard (hibernate.cfg.xml) 
+            // config file.
+            sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            // Log the exception. 
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
 
-	static {
-		try {
-			// Create the SessionFactory from standard (hibernate.cfg.xml)
-			// config file.
-			sessionFactory = new AnnotationConfiguration().configure()
-					.buildSessionFactory();
-		} catch (Throwable ex) {
-			// Log the exception.
-			System.err.println("Initial SessionFactory creation failed." + ex);
-			throw new ExceptionInInitializerError(ex);
-		}
-	}
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
 
-	public static SessionFactory getSessionFactory() {
-		return sessionFactory;
-	}
+    public static Session beginTransaction() {
+        Session hibernateSession = HibernateUtil.getSession();
+        hibernateSession.beginTransaction();
+        return hibernateSession;
+    }
 
-	public static Session beginTransaction() {
-		Session hibernateSession = HibernateUtil.getSession();
-		hibernateSession.beginTransaction();
-		return hibernateSession;
-	}
+    public static void commitTransaction() {
+        HibernateUtil.getSession().getTransaction().commit();
+    }
 
-	public static void commitTransaction() {
-		HibernateUtil.getSession().getTransaction().commit();
-	}
+    public static void rollbackTransaction() {
+        HibernateUtil.getSession().getTransaction().rollback();
+    }
 
-	public static void rollbackTransaction() {
-		HibernateUtil.getSession().getTransaction().rollback();
-	}
+    public static void closeSession() {
+        HibernateUtil.getSession().close();
+    }
 
-	public static void closeSession() {
-		HibernateUtil.getSession().close();
-	}
-
-	public static Session getSession() {
-		Session hibernateSession = sessionFactory.getCurrentSession();
-		return hibernateSession;
-	}
+    public static Session getSession() {
+        Session hibernateSession = sessionFactory.getCurrentSession();
+        return hibernateSession;
+    }
 }
